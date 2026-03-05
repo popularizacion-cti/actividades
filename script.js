9633ffed2017383c8c2084a18041d2a14a79a404
popularizacion-cti <popularizacion@concytec.gob.pe>
2026-03-05 15:04:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -90,7 +90,7 @@ function estiloMapaDinamico(feature) {
 }
 
 function inicializarMapa() {
-    map = L.map('map', { zoomControl: false, zoom: false }).setView([-9.19, -75.015], 5.5);
+    map = L.map('map', { zoomControl: false, minZoom: 5, maxZoom:5 }).setView([-9.19, -75.015], 5);
     L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png').addTo(map);
 
     fetch('peru-regiones.geojson')
